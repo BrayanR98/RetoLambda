cff7a76027da7288995269cf3854b2723975ecf5
BrayanRestrepo <brestrepoisaza@gmail.com>
2022-10-01 12:15:45 -0500

    NTS9-Reto Lambda

---
+++ funcionNumM10.py
@@ -0,0 +1,4 @@
+numeros=[4,10,42,47,9]
+numerosMayores=lambda numero : numero>10
+for i in numeros:
+    print(numerosMayores(i))
---
+++ funcionobjeto.py
@@ -0,0 +1,5 @@
+nombre='brayan'
+edad=12
+ocupacion='asesor'
+generardiccionario=lambda nombre,edad,ocupacion:{'nombre':nombre,'edad':edad,'ocupacion':ocupacion}
+print(generardiccionario(nombre,edad,ocupacion))

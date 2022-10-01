ae73e4a83a6f4cfb26e1d94cc59c7d53ecc3b8f2
209 <209@academicos.cesde.edu.co>
2022-10-01 12:22:35 -0500

    Se solucionan el punto 4 y 5

---
+++ punto4.py
@@ -0,0 +1,3 @@
+#punto 4
+sumarNumeros = lambda numero1,numero2: numero1+numero2
+print(sumarNumeros(5,10))
---
+++ punto5.py
@@ -0,0 +1,5 @@
+#punto 5
+multiplicar = lambda numero : numero * 100
+
+numeroRecibido = int(input("Digite el numero a multiplicar: "))
+print(multiplicar(numeroRecibido))
